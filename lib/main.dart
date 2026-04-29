16d43c1a4aeaacb361f86ab338a15c9dfdc26363
soulahrikermette-hub <soulahrikermette@gmail.com>
2026-04-29 10:29:53 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/screens/login_screen.dart';
 
-// Importa las vistas que creamos (del 49 al 64)
-import 'views/muerto1.dart';
-import 'views/vista_50_reporte_puntualidad.dart';
-// Agrega más vistas según sea necesario
-
 void main() {
   runApp(const SDAGApp());
 }
@@ -17,17 +12,10 @@ class SDAGApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SDAG',
-      theme: AppTheme.lightTheme,
-      // Define las rutas de las vistas
-      routes: {
-        '/login': (context) => const LoginScreen(),  // Ruta de Login
-        '/vista49': (context) => Vista49AutoAsignado(), // Ruta para Vista 49
-        '/vista50': (context) => Vista50ReportePuntualidad(), // Ruta para Vista 50
-        // Agrega más rutas aquí para las vistas del 51 al 64
-      },
-      initialRoute: '/login',  // Inicia en la pantalla de login
-      debugShowCheckedModeBanner: false,
+      title: 'SDAG',  // Título de la app
+      theme: AppTheme.lightTheme,  // Tema claro definido en AppTheme
+      home: const LoginScreen(),  // Pantalla inicial (LoginScreen)
+      debugShowCheckedModeBanner: false,  // Desactiva el banner de depuración
     );
   }
 }
